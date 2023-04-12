9f2dd315df660cea66081b8c6db7981fe0d03bc6
Kanagat01 <111752354+Kanagat01@users.noreply.github.com>
2023-04-12 12:03:39 +0600

    Change logo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return  <nav className="blue darken-4">
         <div className="nav-wrapper">
-            <a href="#!" className="brand-logo">Logo</a>
+            <a href="#!" className="brand-logo">React Movies by Kanagat Sharafiyev</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="#!">Repo</a></li>
             </ul>
